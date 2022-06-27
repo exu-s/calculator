935321d850847af0afeb66ef6361a48c6a02b05d
Sauveur <exaviersauveur@gmail.com>
2022-06-27 10:52:18 -0400

    Done

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -62,7 +62,7 @@ class _HomePageState extends State<HomePage> {
                       alignment: Alignment.centerRight,
                       child: Text(
                         userAnswer,
-                        style: const TextStyle(fontSize: 30),
+                        style: const TextStyle(fontSize: 50),
                       )),
                 ],
               ),
